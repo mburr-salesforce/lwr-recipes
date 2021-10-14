ef44590ed82506b54bc5ecdfb0ae8fd18a25ba24
Vince Chen <vince.chen@salesforce.com>
2021-10-14 15:49:07 -0700

    feat: heroku deployment (#124)

---
+++ scripts/heroku-deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PACKAGES=('base-slds' 'custom-middleware' 'hello-world' 'labels' 'locker' 'module-provider' 'nested-routing' 'routing-extended-metadata' 'security' 'services' 'simple-routing' 'templating')
+
+for package in "${PACKAGES[@]}"
+do
+    heroku_app_name="https://git.heroku.com/lwr-$package.git"
+    git push $heroku_app_name HEAD:master
+done
---
+++ scripts/heroku-deployment-setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+PACKAGES=('base-slds' 'custom-middleware' 'hello-world' 'labels' 'locker' 'module-provider' 'nested-routing' 'routing-extended-metadata' 'security' 'services' 'simple-routing' 'templating')
+
+for package in "${PACKAGES[@]}"
+do
+    app_name="lwr-$package"
+    heroku create -a $app_name
+    heroku buildpacks:add -a $app_name heroku/nodejs
+    heroku buildpacks:add -a $app_name https://github.com/heroku/heroku-buildpack-multi-procfile.git
+    heroku config:set -a $app_name PROCFILE=packages/$package/Procfile
+done
