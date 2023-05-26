27a43a1fe1f8d4fa3474e2881ec5e498b3dd8c6a
Nicolas Kruk <nkruk@salesforce.com>
2023-05-26 19:47:09 -0400

    feat: nested-routing tests (#233)
    * feat: tests basic structure
    
    * animal link selector broken
    
    * fix: nested routing tests
    
    * fix: types file
    
    ---------
    
    Co-authored-by: Shlok Natarajan <shloknatarajan@salesforce.com>

---
+++ packages/nested-routing/test/nested-routing.spec.ts
@@ -0,0 +1,39 @@
+describe('Nested Routing', () => {
+    beforeEach(async () => {
+        await browser.url('/');
+    });
+
+    it('Loads the home page correctly', async () => {
+        // check the title of the page
+        const title = await browser.getTitle();
+        expect(title).toEqual('LWR App');
+
+        // check the page heading
+        const heading = await browser.shadowDeep$('h1');
+        expect(await heading.getText()).toEqual('Home');
+        const subtext = await browser.shadowDeep$('p');
+        expect(await subtext.getText()).toEqual(
+            'This example shows how to route to different pages using multiple nested routers.',
+        );
+    });
+
+    it('Verify nested routing navigation', async () => {
+        // Find the animal nav item
+        const animalsLink = await browser.shadowDeep$('a[href="/animal"]');
+        expect(await animalsLink.getText()).toEqual('Animals');
+
+        // Click the animal nav item
+        await browser.execute('arguments[0].click();', animalsLink);
+
+        // Verify link is displayed
+        const dogLink = await browser.shadowDeep$('a[href="/animal/dog"]');
+        await dogLink.waitForDisplayed();
+
+        // Click the dog link
+        await browser.execute('arguments[0].click();', dogLink);
+
+        // Verify dog picture is displayed
+        const dogPic = await browser.shadowDeep$('img[src="/public/assets/dog.jpeg"]');
+        await dogPic.waitForDisplayed();
+    });
+});
