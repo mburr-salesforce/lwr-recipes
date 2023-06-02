23ef36379654ae42104f58ae7b4446d1951145fc
Shlok Natarajan <shloknatarajan@salesforce.com>
2023-06-02 09:05:57 -0700

    feat: Completed tests for nested routing (#236)
    * fix: cat tests
    
    * fix: added url verification to tests
    
    ---------
    
    Co-authored-by: Nicolas Kruk <nkruk@salesforce.com>

--- packages/nested-routing/test/nested-routing.spec.ts
+++ packages/nested-routing/test/nested-routing.spec.ts
@@ -21,19 +21,34 @@ describe('Nested Routing', () => {
         // Find the animal nav item
         const animalsLink = await browser.shadowDeep$('a[href="/animal"]');
         expect(await animalsLink.getText()).toEqual('Animals');
+        expect(await browser.getUrl()).toEqual('http://localhost:8080/');
 
         // Click the animal nav item
         await browser.execute('arguments[0].click();', animalsLink);
+        expect(await browser.getUrl()).toEqual('http://localhost:8080/animal');
 
-        // Verify link is displayed
+        // Verify the dog link is displayed
         const dogLink = await browser.shadowDeep$('a[href="/animal/dog"]');
         await dogLink.waitForDisplayed();
 
         // Click the dog link
         await browser.execute('arguments[0].click();', dogLink);
+        expect(await browser.getUrl()).toEqual('http://localhost:8080/animal/dog');
 
         // Verify dog picture is displayed
         const dogPic = await browser.shadowDeep$('img[src="/public/assets/dog.jpeg"]');
         await dogPic.waitForDisplayed();
+
+        // Verify the cat link is displayed
+        const catLink = await browser.shadowDeep$('a[href="/animal/cat"]');
+        await catLink.waitForDisplayed();
+
+        // Click the cat link
+        await browser.execute('arguments[0].click();', catLink);
+        expect(await browser.getUrl()).toEqual('http://localhost:8080/animal/cat');
+
+        // Verify cat picture is displayed
+        const catPic = await browser.shadowDeep$('img[src="/public/assets/cat.jpeg"]');
+        await catPic.waitForDisplayed();
     });
 });
