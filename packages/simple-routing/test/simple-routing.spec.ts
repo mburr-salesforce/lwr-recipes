544f03e5bb8e213099fa44e538eaf0bc6252f86e
Nicolas Kruk <nkruk@salesforce.com>
2023-06-02 12:20:02 -0400

    feat: completed tests for simple-routing (#238)
    * fix: started applying changes from nested to simple routing
    
    * fix: chromedriver and simple-routing test update
    
    * fix: properly debug tests
    
    * fix: timeouts
    
    * feat: testing for all pages
    
    * fix: downgraded chromedriver to 112
    
    * fix: downgraded chromedriver so all are on 112
    
    * chore: rename debug command
    
    * fix: update server mode back to prod
    
    * feat: Completed tests for nested routing (#236)
    
    * fix: cat tests
    
    * fix: added url verification to tests
    
    ---------
    
    Co-authored-by: Nicolas Kruk <nkruk@salesforce.com>
    
    * chore: cleanup
    
    ---------
    
    Signed-off-by: Nicolas Kruk <nkruk@salesforce.com>
    Co-authored-by: Shlok Natarajan <shloknatarajan@salesforce.com>

--- packages/simple-routing/test/simple-routing.spec.ts
+++ packages/simple-routing/test/simple-routing.spec.ts
@@ -19,4 +19,38 @@ describe('app navbar navigation', () => {
             'The Lightning Web Runtime Farm is a sustainable family farm',
         );
     });
+
+    it('Verify simple routing navigation', async () => {
+        // Find the products nav item
+        const productsLink = await browser.shadowDeep$('a[href="/products"]');
+        await productsLink.waitForDisplayed();
+        expect(await productsLink.getText()).toEqual('Products');
+
+        // Click the products nav item
+        await browser.execute('arguments[0].click();', productsLink);
+        const productsHeading = await browser.shadowDeep$('h1[class="products"]');
+        expect(await productsHeading.getText()).toEqual('Our Products');
+
+        // Click the recipes nav item
+        const recipesLink = await browser.shadowDeep$('a[href="/recipes"]');
+        await recipesLink.waitForDisplayed();
+        await browser.execute('arguments[0].click();', recipesLink);
+
+        const recipesPic = await browser.shadowDeep$('img[src="/public/assets/cook.jpg"]');
+        await recipesPic.waitForDisplayed();
+
+        // Click the contact nav item
+        const contactLink = await browser.shadowDeep$('a[href="/contact"]');
+        await contactLink.waitForDisplayed();
+        await browser.execute('arguments[0].click();', contactLink);
+        const textArea = await browser.shadowDeep$('textarea');
+        await textArea.waitForDisplayed();
+
+        // Click the contact nav item
+        const errorLink = await browser.shadowDeep$('a[href="/pageHasError"]');
+        await errorLink.waitForDisplayed();
+        await browser.execute('arguments[0].click();', errorLink);
+        const errorButton = await browser.shadowDeep$('button');
+        await errorButton.waitForDisplayed();
+    });
 });
