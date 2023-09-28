fffa15b06697dc4355d1c67e13e1be17ab48f1d3
Shlok Natarajan <shloknatarajan@salesforce.com>
2023-09-28 13:07:08 -0700

    feat: metrics count dynamic load counts (#275)
    * feat: dynamic counts using lwr load log
    
    * feat: updated for local additions to lwr/metrics
    
    * fix: removed local links from package json
    
    * fix: package json
    
    * feat: updated string processing to match lwr update
    
    * feat: updated lwr version in metrics
    
    * fix: tsignore update and alpha.6->alpha.7
    
    * feat: removed extra sent metrics

--- packages/metrics/src/index.ts
+++ packages/metrics/src/index.ts
@@ -9,13 +9,19 @@ app.use(express.json());
 // Add middleware to accept mock metric APIs
 // In production, the data would be saved to a database or elsewhere
 const moduleCounts: { [key: string]: number } = {};
+const dynamicCounts: { [key: string]: number } = {};
 app.post('/lwr/metrics', async (req, res) => {
     const data = req.body;
     for (const key in data.nameCounts) {
         moduleCounts[key] = (moduleCounts[key] || 0) + data.nameCounts[key];
     }
+
+    for (const key in data.dynamicNameCounts) {
+        dynamicCounts[key] = (dynamicCounts[key] || 0) + data.dynamicNameCounts[key];
+    }
     console.log('LWR Metrics: ', data, '\n');
     console.log('Module Counts', moduleCounts);
+    console.log('Dynamically Loaded Counts', dynamicCounts);
     res.status(200).send();
 });
 
--- packages/metrics/src/modules/example/metricsSink/metricsSink.ts
+++ packages/metrics/src/modules/example/metricsSink/metricsSink.ts
@@ -1,14 +1,23 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import { BOOTSTRAP_PREFIX, BOOTSTRAP_END, BOOTSTRAP_ERROR, INIT_MODULE, MODULE_DEFINE } from 'lwr/metrics';
-
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import {
+    BOOTSTRAP_PREFIX,
+    BOOTSTRAP_END,
+    BOOTSTRAP_ERROR,
+    INIT_MODULE,
+    MODULE_DEFINE,
+    MODULE_DYNAMIC_LOAD, // @ts-ignore
+} from 'lwr/metrics';
 export default function (): void {
     if (PerformanceObserver) {
         let existingMarks: PerformanceEntry[] | null;
+        let existingDynamicMarks: PerformanceEntry[] | null;
         if (performance) {
             existingMarks = performance
                 .getEntriesByType('mark')
                 .filter((e) => e.name.startsWith(MODULE_DEFINE));
+            existingDynamicMarks = performance
+                .getEntriesByType('mark')
+                .filter((e) => e.name.startsWith(MODULE_DYNAMIC_LOAD));
         }
 
         // Watch for new metrics
@@ -16,6 +25,7 @@ export default function (): void {
             // Gather all the bootstrap marks
             const bootstrapMarks = list.getEntries().filter((e) => e.name.startsWith(BOOTSTRAP_PREFIX));
             const moduleMarks = list.getEntries().filter((e) => e.name.startsWith(MODULE_DEFINE));
+            const dynamicMarks = list.getEntries().filter((e) => e.name.startsWith(MODULE_DYNAMIC_LOAD));
             // Get bootstrap end count (should be 0 or 1)
             const bootstrapEndCount = bootstrapMarks.reduce((count, mark) => {
                 return mark.name === BOOTSTRAP_END ? count + 1 : count;
@@ -35,6 +45,9 @@ export default function (): void {
             const processMarkNames = (markName: string): string => {
                 return markName.replace(/lwr\.loader\.module\.define-|\/v\/.*/g, '');
             };
+            const processDynamicMarkNames = (markName: string): string => {
+                return markName.replace(/lwr\.loader\.moduleRegistry\.dynamicLoad-|\/v\/.*/g, '');
+            };
 
             let moduleMarkNames: string[] = moduleMarks.map((mark) => processMarkNames(mark.name));
             if (existingMarks) {
@@ -42,9 +55,23 @@ export default function (): void {
                 moduleMarkNames = [...existingMarkNames, ...moduleMarkNames];
                 existingMarks = null;
             }
+            let dynamicMarkNames: string[] = dynamicMarks.map((mark) => processDynamicMarkNames(mark.name));
+            if (existingDynamicMarks) {
+                const existingDynamicMarkNames: string[] = existingDynamicMarks.map((mark) =>
+                    processDynamicMarkNames(mark.name),
+                );
+                dynamicMarkNames = [...existingDynamicMarkNames, ...dynamicMarkNames];
+                existingDynamicMarks = null;
+            }
+
             const nameCounts: { [key: string]: number } = {};
             moduleMarkNames.forEach((e: string) => {
                 nameCounts[e] = (nameCounts[e] || 0) + 1;
+            });
+
+            const dynamicNameCounts: { [key: string]: number } = {};
+            dynamicMarkNames.forEach((e: string) => {
+                dynamicNameCounts[e] = (dynamicNameCounts[e] || 0) + 1;
             });
             // Send the metrics to the server to be [mock] recorded
             fetch('/lwr/metrics', {
@@ -55,6 +82,7 @@ export default function (): void {
                     bootstrapErrorCount,
                     moduleInitCount,
                     nameCounts,
+                    dynamicNameCounts,
                 }),
             });
         });
